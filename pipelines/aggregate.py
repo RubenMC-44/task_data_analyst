82c1960e8d5d84cdbea8c94f8af6799aa0020f64
RubenMC-44 <fanatikps4@gmail.com>
2026-06-02 10:09:52 -0400

    feat: add per-minute aggregation metrics

---
+++ pipelines/aggregate.py
@@ -0,0 +1,25 @@
+import pandas as pd
+
+def aggregate_by_minute(df: pd.DataFrame) -> pd.DataFrame:
+    df = df.copy()
+    df["minute"] = df["time_stamp"].dt.floor("min")
+    #df["laser_n1_alarm"] = df["laser_n1_main_status"] & 1
+    #df["laser_s1_alarm"] = df["laser_s1_main_status"] & 1
+    metrics_per_minute = df.groupby(["session_id","minute"]).agg(
+        gps_avg_speed = ("gps_speed","mean"),
+        gps_count_no_signal =("gps_n_satellites",lambda x: (x == 0).sum()),
+        gps_avg_quality = ("gps_quality", "mean"),
+        avg_blower_pressure = ("blower_pressure","mean"),
+        max_blower_pressure = ("blower_pressure","max"),
+        avg_compressor_pressure = ("compressor_pressure","mean"),
+        max_compressor_pressure = ("compressor_pressure","max"),
+        laser_n1_avg_power = ("laser_n1_measured_power", "mean"),
+        #laser_n1_alarm_count = ("laser_n1_alarm", "sum"),
+        laser_s1_avg_power = ("laser_s1_measured_power", "mean"),
+        #laser_s1_alarm_count = ("laser_s1_alarm", "sum"),
+        avg_temp_lens_box_n1 = ("temp_n1_lens_box","mean"),
+        avg_temp_lens_box_s1 = ("temp_s1_lens_box","mean"),
+        avg_system_health_cpu = ("system_health_cpu","mean"),
+        avg_system_health_free_memory = ("system_health_free_memory","mean")
+    )
+    return metrics_per_minute.reset_index()
